7b823336fe01bee1909dc285b2e9410e3568b659
Denys Dudin <denisdudin50@gmail.com>
2020-08-29 09:27:44 +0300

    Adding of the Order_Section

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { NavBar } from './Components/NavBar';
 import { Menu } from './Components/Menu';
 import { GlobalStyle } from './Components/GlobalStyle';
 import { ModalItem } from './Components/ModalItem';
+import { Order } from './Components/Order';
 
 function App() {
 
@@ -13,7 +14,8 @@ function App() {
   return (
     <>
       <GlobalStyle/>
-      <NavBar/> 
+      <NavBar/>
+      <Order/> 
       <Menu setOpenItem={setOpenItem} /> 
       <ModalItem openItem={openItem} 
                 setOpenItem={setOpenItem}/>          
