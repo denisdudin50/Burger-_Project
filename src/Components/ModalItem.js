34f1af9b9c94fe84c8e37188c2fa8c1d92cca717
Denys Dudin <denisdudin50@gmail.com>
2020-08-29 07:17:09 +0300

    Adding of the button in modalItem

---
+++ src/Components/ModalItem.js
@@ -0,0 +1,79 @@
+import React from 'react';
+import styled from 'styled-components';
+import { ButtonCheckout } from './ButtonCheckout';
+
+const Overlay = styled.div`
+    position: fixed;
+    top: 0;
+    left: 0;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(0,0,0, .5);
+    z-index: 20;
+`;
+
+const Modal = styled.div`
+    background-color: #fff;
+    width: 600px;
+    height: 600px;
+`;
+
+const Banner = styled.div`
+    width: 100%;
+    height: 200px;
+    background-image: url(${({img}) => img});
+    background-size: cover;
+    background-position: center;
+    
+`;
+
+const Content = styled.section`
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    height: 67%;
+    padding: 30px;
+
+`;
+
+const HeaderContent = styled.div`
+    display: flex;
+    justify-content: space-between;
+    font-size: 24px;
+    font-weight: 700;
+    font-family: 'Pacifico', cursive;
+`;
+
+
+
+export const ModalItem = ({ openItem, setOpenItem }) => {
+ 
+    function closeModal(e) {
+        if(e.target.id === 'overlay')
+        setOpenItem(null);
+    }
+
+    if(!openItem)
+    return null;
+  return (
+    <Overlay id="overlay" onClick={closeModal}>
+
+        <Modal>
+            <Banner img={openItem.img}/>
+            <Content>
+               <HeaderContent>
+                     <div>{openItem.name}</div>
+                     <div>{openItem.price}</div>
+
+                </HeaderContent> 
+                <ButtonCheckout>Добавить</ButtonCheckout>
+            </Content>
+            
+        </Modal>
+
+    </Overlay>
+  )
+};
